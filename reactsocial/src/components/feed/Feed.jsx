206effdb959434d85570d0f3bd94d798d9616b19
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-01-26 22:09:50 +0545

    still post feed not solved

--- reactsocial/src/components/feed/Feed.jsx
+++ reactsocial/src/components/feed/Feed.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import "./feed.css"
 import Share from '../share/Share'
-import Post from '../post/Post'
-import {Posts} from "../../dummyData"
+ import Post from '../post/Post'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+// import {Posts} from "../../dummyData"
+
 export default function Feed() {
+  const [posts, setPosts] =  useState([]);
+useEffect(()=>{
+  const fetchPosts = async() =>{
+    console.log("feed rendered")
+    const res = await axios.get("http://localhost:8800/api/posts/63cfdb135daafb574ca7e242");
+    setPosts(res.data)
+    console.log(res)
+  };
+  fetchPosts();
+},[])
+// http://localhost:8800/api/posts/timeline/63cfcaf66f001c908291fcab
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <Share/>
-        {Posts.map((p)=>{
-          return(
-            <Post key={p.id} post={p}/>
-        )
-         
-        })}
+        {posts.map((p)=>{
         
-       
+            <Post key={p.id} post={p}/>
+        
+        })}
       </div>
     </div>
   )
