2120b46702b391876996801329b73f5a18b600e9
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-02-25 11:15:10 +0545

    fetched news feed

--- reactsocial/src/components/feed/Feed.jsx
+++ reactsocial/src/components/feed/Feed.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import "./feed.css"
-import Share from '../share/Share'
- import Post from '../post/Post'
-import { useEffect, useState } from 'react'
-import axios from "axios"
+import React from "react";
+import "./feed.css";
+import Share from "../share/Share";
+import Post from "../post/Post";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // import {Posts} from "../../dummyData"
 
 export default function Feed() {
-  const [posts, setPosts] =  useState([]);
-useEffect(()=>{
-  const fetchPosts = async() =>{
-    console.log("feed rendered")
-    const res = await axios.get("http://localhost:8800/api/posts/63cfdb135daafb574ca7e242");
-    setPosts(res.data)
-    console.log(res)
-  };
-  fetchPosts();
-},[])
-// http://localhost:8800/api/posts/timeline/63cfcaf66f001c908291fcab
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      console.log("feed rendered");
+      const res = await axios.get(
+        "http://localhost:8800/api/posts/timeline/63cfcaf66f001c908291fcab"
+      );
+      setPosts(res.data);
+      console.log(posts);
+      console.log(res.data);
+    };
+    fetchPosts();
+  }, []);
+  // http://localhost:8800/api/posts/timeline/63cfcaf66f001c908291fcab
   return (
-    <div className='feed'>
+    <div className="feed">
       <div className="feedWrapper">
-        <Share/>
-        {posts.map((p)=>{
-        
-            <Post key={p.id} post={p}/>
-        
-        })}
+        <Share />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- reactsocial/src/components/post/Post.jsx
+++ reactsocial/src/components/post/Post.jsx
@@ -1,40 +1,37 @@
 import React from "react";
 import "./post.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-// import {Users} from "../../dummyData"
 import { useState, useEffect } from "react";
 import axios from "axios";
-export default function Post({post}) {
-  const [like, setLike] = useState(post.likes.length)
-  const [isLiked, setIsLiked] = useState(false)
-  const [user, setUser] = useState({})
+export default function Post({ post }) {
+  const [like, setLike] = useState(post.likes.length);
+  const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
 
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-  useEffect(()=>{
-    const fetchUser = async() =>{
-      const res = await axios.get(`http://localhost:8800/api/users/${post.userId}`);
-      setUser(res.data)
-      console.log(res)
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(
+        `http://localhost:8800/api/users/${post.userId}`
+      );
+      setUser(res.data);
+      console.log(res);
     };
     fetchUser();
-  },[])
+  }, []);
 
-console.log(PF)
-  const likeHandler = ()=>{
-    setLike(isLiked ? like -1: like+1)
-    setIsLiked(!isLiked)
-  }
+  console.log(PF);
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              className="postProfileImg" 
-              src={user.profilePicture || PF+"person/noAvatar.jpeg"}
-              alt=""
-            />
+            <img className="postProfileImg" src={user.profilePicture} alt="" />
             <span className="postUsername">{user.username}</span>
             <span className="postDate">{post.date}</span>
           </div>
@@ -44,12 +41,22 @@ console.log(PF)
         </div>
         <div className="postCenter">
           <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF+post.img} alt="" />
+          <img className="postImg" src={PF + post.img} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src={`${PF}like.png`} onClick={likeHandler} alt=""></img>
-            <img className="likeIcon" src={`${PF}heart.png`} onClick={likeHandler} alt=""></img>
+            <img
+              className="likeIcon"
+              src={`${PF}like.png`}
+              onClick={likeHandler}
+              alt=""
+            ></img>
+            <img
+              className="likeIcon"
+              src={`${PF}heart.png`}
+              onClick={likeHandler}
+              alt=""
+            ></img>
             <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
